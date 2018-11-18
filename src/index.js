ead1d3f22780f247a3f522f351d029cb9d9e8cbd
Maksim Chemerisuk <chemerisuk@gmail.com>
2018-11-18 08:28:48 +0300

    bug fixes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 const fs = require("fs");
-const p = require("p");
+const p = require("path");
 
-const postcssJs = require("postcss-js");
+const postcss = require("postcss");
 const autoprefixer = require("autoprefixer");
 const csswring = require("csswring");
-const customProperties = require("postcss-custom-properties");
+const cssvariables = require("postcss-css-variables");
 
 const browsers = ["ChromeAndroid 30", "iOS 7", "IE 10"];
-const prefixer = postcssJs.sync([ customProperties(), autoprefixer({browsers}), csswring ]);
-
-function endsWith(str, search) {
-  return str.indexOf(search, str.length - search.length) !== -1;
-}
+const prefixer = postcss([ cssvariables(), autoprefixer({browsers}), csswring ]);
 
 module.exports = babel => {
   const t = babel.types;
@@ -22,15 +18,16 @@ module.exports = babel => {
         exit(path, state) {
           const node = path.node;
 
-          if (endsWith(node.source.value, '.css')) {
+          if (node.source.value.slice(-4) === ".css") {
             const dir = p.dirname(p.resolve(state.file.opts.filename));
             const absolutePath = p.resolve(dir, node.source.value);
-            const css = prefixer(fs.readFileSync(absolutePath, "utf8"));
+            const cssText = fs.readFileSync(absolutePath, "utf8");
+            const result = prefixer.process(cssText);
 
             path.replaceWith(t.variableDeclaration("var", [
               t.variableDeclarator(
                 t.identifier(node.specifiers[0].local.name),
-                t.stringLiteral(css))]));
+                t.stringLiteral(result.css))]));
           }
         }
       }
